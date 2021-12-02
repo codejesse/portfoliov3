3923d5193093ec16c3ce31b58615826e99eaece0
Doris Ngere <jessebeke10@gmail.com>
2021-12-02 03:44:13 +0100

    portfolio site commit

---
+++ pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import link from "next/link";
+
+const Contact = () => {
+  return (
+    <div>
+      <Link href="mailto:jessebeke10@gmail.com" passHref>
+        <h1>Jessebeke10@gmail.com</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Contact;
